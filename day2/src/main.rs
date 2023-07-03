4bfb122067db68d8b16bf0c55af8ee359393dd82
Alexander Black <jermakov.aleksandr@gmail.com>
2023-07-03 17:55:10 +0100

    Day 2, part 1

---
+++ day2/src/main.rs
@@ -0,0 +1,48 @@
+use std::collections::HashMap;
+use itertools::Itertools;
+const INPUT: &str = include_str!("../data/input.txt");
+
+enum Symbol {
+    Rock,
+    Paper,
+    Scissors,
+}
+
+fn part1(symbols: &HashMap<&str, Symbol>) -> u32 {
+    let mut opponent: &str;
+    let mut me: &str;
+    let mut score: u32 = 0;
+
+    for line in INPUT.lines() {
+        (opponent, me) = line.split_whitespace().next_tuple().unwrap();
+        let me_symbol = symbols.get(&me).unwrap();
+        let opponent_symbol = symbols.get(&opponent).unwrap();
+
+        match me_symbol {
+            Symbol::Rock => score += 1,
+            Symbol::Paper => score += 2,
+            Symbol::Scissors => score += 3,
+        }
+
+        match (me_symbol, opponent_symbol) {
+            (Symbol::Rock, Symbol::Scissors) => score += 6,
+            (Symbol::Rock, Symbol::Paper) => score += 0,
+            (Symbol::Paper, Symbol::Rock) => score += 6,
+            (Symbol::Paper, Symbol::Scissors) => score += 0,
+            (Symbol::Scissors, Symbol::Paper) => score += 6,
+            (Symbol::Scissors, Symbol::Rock) => score += 0,
+            _ => score += 3,
+        }
+    }
+
+    score
+}
+    
+fn main() {
+	let symbols = HashMap::from([("A",Symbol::Rock),
+	    ("B", Symbol::Paper), 
+	    ("C", Symbol::Scissors),
+	    ("X", Symbol::Rock), 
+	    ("Y", Symbol::Paper), ("Z", Symbol::Scissors)]);
+	println!("Score: {}", part1(&symbols));
+}
