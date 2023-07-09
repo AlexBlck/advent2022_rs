22077f33c1bc13e0c08b740efb2cd29a7715a3fc
Alexander Black <alex@gentian.team>
2023-07-09 18:35:50 +0100

    Day 6, part 2 (and refactor)

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,26 +1,24 @@
 const INPUT: &str = include_str!("../data/input.txt");
 
 
-fn part1(input: &str) -> u32 {
-    for idx in 4..input.len() {
-        let slice:&str = &input[idx-4..idx];
-        // number of unique characters in slice
+fn find_first_n_distinct(input: &str, n: usize) -> u32 {
+    for idx in n..input.len() {
+        let slice:&str = &input[idx-n..idx];
         let mut unique_chars: Vec<char> = slice.chars().collect();
         unique_chars.sort();
         unique_chars.dedup();
-        if unique_chars.len() == 4 {
+        if unique_chars.len() == n {
             return idx as u32;
         }
     }
     0
 }
 
-
-
 fn main() {
-    let part1_answer: u32 = part1(INPUT);
+    let part1_answer: u32 = find_first_n_distinct(INPUT, 4);
+    let part2_answer: u32 = find_first_n_distinct(INPUT, 14);
     println!("Part 1 answer: {}", part1_answer);
-    // part2();
+    println!("Part 2 answer: {}", part2_answer);
 }
 
 #[cfg(test)]
@@ -29,18 +27,39 @@ mod tests {
 
     #[test]
     fn test_part1_case1() {
-        assert_eq!(part1("bvwbjplbgvbhsrlpgdmjqwftvncz"), (5));
+        assert_eq!(find_first_n_distinct("bvwbjplbgvbhsrlpgdmjqwftvncz", 4), (5));
     }
     #[test]
     fn test_part1_case2() {
-        assert_eq!(part1("nppdvjthqldpwncqszvftbrmjlhg"), (6));
+        assert_eq!(find_first_n_distinct("nppdvjthqldpwncqszvftbrmjlhg", 4), (6));
     } 
     #[test]
     fn test_part1_case3() {
-        assert_eq!(part1("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"), (10));
+        assert_eq!(find_first_n_distinct("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 4), (10));
     }
     #[test]
     fn test_part1_case4() {
-        assert_eq!(part1("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"), (11));
+        assert_eq!(find_first_n_distinct("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 4), (11));
+    }
+    
+    #[test]
+    fn test_part2_case0() {
+        assert_eq!(find_first_n_distinct("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 14), (19));
+    }
+    #[test]
+    fn test_part2_case1() {
+        assert_eq!(find_first_n_distinct("bvwbjplbgvbhsrlpgdmjqwftvncz", 14), (23));
+    }
+    #[test]
+    fn test_part2_case2() {
+        assert_eq!(find_first_n_distinct("nppdvjthqldpwncqszvftbrmjlhg", 14), (23));
+    } 
+    #[test]
+    fn test_part2_case3() {
+        assert_eq!(find_first_n_distinct("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 14), (29));
+    }
+    #[test]
+    fn test_part2_case4() {
+        assert_eq!(find_first_n_distinct("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 14), (26));
     }
 }
